3adcf050f73d1c089cf8b10ffa09878ff8932e23
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-15 11:20:04 -0500

    add slide logic in state

--- src/components/Dating.js
+++ src/components/Dating.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import { data } from "../data/data";
+import s from "./Dating.module.css";
+
 export function Dating() {
+  const [person, setPerson] = useState(0);
+  const { id, name, description, age, image } = data[person];
+  console.log(data[person]);
+
   return (
-    <div>
-      <h1>TEST</h1>
+    <div className={s.container}>
+      <h1>{name}</h1>
     </div>
   );
 }
